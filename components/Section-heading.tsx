c7f2be4be078d2eee5ff5807bb500eb840425320
Dikshant441 <singhdikshant200@gmail.com>
2024-06-06 10:50:00 +0530

    create an About section

---
+++ components/Section-heading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type SectionHeadingProp = {
+    children: React.ReactNode
+}
+
+export default function SectionHeading({ children }: SectionHeadingProp) {
+    return (
+        <h2 className="text-3xl font-medium capitalize mb-8 text-center">
+            {children}
+        </h2>
+    );
+}
